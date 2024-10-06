797d5edf3bb3a97d6e30c3ca81252716f8e4354a
john <jburton8484@gmail.com>
2024-10-06 14:45:43 -0400

    updated list and linked list files

--- linked_list.py
+++ linked_list.py
@@ -1,51 +1,96 @@
-# Constructing a Linked List
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 class LinkedList:
-    def __init__(self):
-        self.head = None
-linked_list = LinkedList()
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
-# Creating nodes
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
-# Linking nodes
-node1.next = node2
-node2.next = node3
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
-# Setting head
-linked_list.head = node1
+    def update(self, index, value):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        count = 0
+        while count < index:
+            temp = temp.next
+            count += 1
+        if temp:
+            temp.value = value
+            return True
+        return False
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+# Construct first node of linked list
+linked_list = LinkedList(1)
+print('Construct first node:')
 print(linked_list.head.value)
-print(linked_list.head.next.value)
-print(linked_list.head.next.next.value)
 
-
-# Add
-new_node = Node(4)
-node3.next = new_node
+# Add five more nodes
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+linked_list.append(6)
 print('Add:')
 print(linked_list.head.value)
 print(linked_list.head.next.value)
 print(linked_list.head.next.next.value)
 print(linked_list.head.next.next.next.value)
+print(linked_list.head.next.next.next.next.value)
+print(linked_list.head.next.next.next.next.next.value)
 
-
-# Remove
-node1.next = node3
+# Remove last node
+linked_list.pop()
 print('Remove:')
 print(linked_list.head.value)
 print(linked_list.head.next.value)
 print(linked_list.head.next.next.value)
-
+print(linked_list.head.next.next.next.value)
+print(linked_list.head.next.next.next.next.value)
 
 # Update
-node3.value = 10
+linked_list.update(2, 56)
 print('Update:')
 print(linked_list.head.value)
 print(linked_list.head.next.value)
 print(linked_list.head.next.next.value)
+print(linked_list.head.next.next.next.value)
+print(linked_list.head.next.next.next.next.value)
+
+
---
+++ list.py
@@ -0,0 +1,54 @@
+class List:
+    def __init__(self):
+        self.data = []
+
+    def construct(self, items):
+        self.data = []
+        for item in items:
+            self.data += [item]
+
+    def append(self, item):
+        self.data = self.data + [item]
+
+    def remove(self, item):
+        new_data = []
+        found = False
+        for i in self.data:
+            if i == item and not found:
+                found = True
+            else:
+                new_data += [i]
+        self.data = new_data
+
+    def update(self, index, value):
+        count = 0
+        for idx in self.data:
+            if count == index:
+                self.data = self.data[:count] + [value] + self.data[count+1:]
+                break
+            count += 1
+        else:
+            print('Index out of range.')
+
+
+# Construct the list
+my_list = List()
+my_list.construct([1, 2, 3, 4])
+print('Construct:')
+print(my_list.data)
+
+# append an item
+my_list.append(5)
+print('Add:')
+print(my_list.data)
+
+# Remove an item
+my_list.remove(3)
+print('Remove:')
+print(my_list.data)
+
+# Update an item
+my_list.update(2, 10)
+print('Update:')
+print(my_list.data)
+
